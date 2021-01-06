d275422caf8c9a28e91260b23e86a2875d315568
Mat-ling From Android <yaosite@139.com>
2021-01-06 19:52:14 +0800

    modify logic

---
+++ disp-default.c
@@ -0,0 +1,6 @@
+/** disp-default.c - Default displayer.
+ * When &flush() is called, we merge everything in the Queue and generate 
+ */
+
+
+
---
+++ rule-default.c
@@ -0,0 +1,6 @@
+/** rule-default.c - Default Gamerule.
+ * Gamerules other than this is loaded by DLL.
+ */
+
+#include "game-def.h"
+
